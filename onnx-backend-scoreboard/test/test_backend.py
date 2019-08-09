cdf047964a4bf1c6d09714a9aa3d9069651e27db
mitruska <katarzyna.mitrus@intel.com>
2019-08-09 13:26:26 +0200

    Update test code

--- onnx-backend-scoreboard/test/test_backend.py
+++ onnx-backend-scoreboard/test/test_backend.py
@@ -1,16 +1,24 @@
+"""ONNX backend test initialization."""
+
 import importlib
-import sys
 import test
 import unittest
 
 import onnx.backend.test
 
 
-# Custom backend importer
 def import_backend(onnx_backend_module):
+    """Import ONNX backend module.
+
+    :param onnx_backend_module: ONNX backend module to import.
+    :type onnx_backend_module: str
+    :raises ValueError: Not a valid ONNX backend.
+    :return: The ONNX backend module.
+    :rtype: class 'module'
+    """
     backend = importlib.import_module(onnx_backend_module)
-    if not hasattr(backend, 'run_model') and not hasattr(backend, 'run'):
-        raise ValueError('%s is not a valid ONNX backend', onnx_backend_module)
+    if not hasattr(backend, "run_model") and not hasattr(backend, "run"):
+        raise ValueError("%s is not a valid ONNX backend", onnx_backend_module)
     return backend
 
 
@@ -18,16 +26,16 @@ def import_backend(onnx_backend_module):
 backend = import_backend(test.ONNX_BACKEND_MODULE)
 
 # Set backend device name to be used
-backend.backend_name = 'CPU'
+backend.backend_name = "CPU"
 
 # This is a pytest variable to load extra plugins
 # Enable the ONNX compatibility report
-pytest_plugins = 'onnx.backend.test.report'
+pytest_plugins = "onnx.backend.test.report"
 
 # Import all test cases at global scope to make them visible to python.unittest
 backend_test = onnx.backend.test.BackendTest(backend, __name__)
 globals().update(backend_test.enable_report().test_cases)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
