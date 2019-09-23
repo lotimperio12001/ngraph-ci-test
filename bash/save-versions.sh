6b69c3c6cfee19748aac7cb22b80b2b031f0501d
mitruska <katarzyna.mitrus@intel.com>
2019-09-23 15:33:48 +0200

    Next env test

--- bash/save-versions.sh
+++ bash/save-versions.sh
@@ -2,6 +2,7 @@
 echo "Save versions script!"
 cd ~
 mkdir /root/versions
-export VERSIONS_DIR="/root/versions"
+env VERSIONS_DIR="/root/versions"
 pip list --format=json > /root/versions/pip-list.json
 cat /root/versions/pip-list.json
+printenv
