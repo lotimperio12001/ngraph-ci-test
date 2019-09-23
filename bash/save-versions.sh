ae32db8ad602b656e276266005d0c8c4da9cdde3
mitruska <katarzyna.mitrus@intel.com>
2019-09-23 15:51:34 +0200

    Env list file

--- bash/save-versions.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Save versions script!"
-cd ~
-mkdir /root/versions
-env VERSIONS_DIR="/root/versions"
-pip list --format=json > /root/versions/pip-list.json
-cat /root/versions/pip-list.json
-printenv
