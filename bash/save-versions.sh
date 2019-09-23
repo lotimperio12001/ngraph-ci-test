2fc5a1b7840b1a894e5c41e220dd1dc7aaaea8f1
mitruska <katarzyna.mitrus@intel.com>
2019-09-23 15:14:38 +0200

    Debug export env var

--- bash/save-versions.sh
+++ bash/save-versions.sh
@@ -2,6 +2,6 @@
 echo "Save versions script!"
 cd ~
 mkdir /root/versions
-export VERSIONS_DIR=/root/versions
+export VERSIONS_DIR="/root/versions"
 pip list --format=json > /root/versions/pip-list.json
 cat /root/versions/pip-list.json
