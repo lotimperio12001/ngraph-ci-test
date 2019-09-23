a59ed50e7cef9d06befbe917f999b3b22fbcc891
mitruska <katarzyna.mitrus@intel.com>
2019-09-23 15:02:05 +0200

    Run save version script

--- bash/save-versions.sh
+++ bash/save-versions.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo "Save versions script!"
 cd ~
 mkdir /root/versions
 export VERSIONS_DIR=/root/versions
+pip list --format=json > /root/versions/pip-list.json
+cat /root/versions/pip-list.json
