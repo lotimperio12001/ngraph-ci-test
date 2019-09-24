5e7ffc72a264fb442e980f94a17b18096f4869c2
mitruska <katarzyna.mitrus@intel.com>
2019-09-24 10:50:26 +0200

    ONNX Runtime new Dockerfile test

--- setups/docker-setup.sh
+++ setups/docker-setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 cd ~
 mkdir versions
-mkdir test
 mkdir results
 
 . /root/setups/save-versions.sh
--- setups/save-versions.sh
+++ setups/save-versions.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-echo "Save versions script!"
 cd ~
 mkdir /root/versions
 pip list --format=json > /root/versions/pip-list.json
 cat /root/versions/pip-list.json
-printenv
