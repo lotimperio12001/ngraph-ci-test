59eafd41d033ddc94b670d55d9a7658d20565db4
mitruska <katarzyna.mitrus@intel.com>
2019-10-09 16:32:58 +0200

    Frontend update, add searching on table

--- docs/resources/src/main.js
+++ docs/resources/src/main.js
@@ -95,3 +95,27 @@ new Chart(bar_chart, {
         }
     }
 });
+
+// Filter tables
+let onSearch = (inputId, tableId) => {
+    let input = document.getElementById(inputId);
+    let filter = input.value.toLowerCase();
+    let table = document.getElementById(tableId);
+    let testNames = table.getElementsByClassName("testName")
+    let allFrameworkResults = table.getElementsByClassName("frameworkResults");
+
+    for (let i=0; i < testNames.length; i++) {
+        let testNameText = testNames[i].textContent || testNames[i].innerText;
+        for (let j = 0; j < allFrameworkResults.length; j++) {
+            let frameworkResults = allFrameworkResults[j].getElementsByClassName("testResult");
+            if (testNameText.toLowerCase().indexOf(filter) < 0) {
+                testNames[i].style.display = "none";
+                frameworkResults[i].style.display = "none";
+            }
+            else {
+                testNames[i].style.display = "";
+                frameworkResults[i].style.display = "";
+            }
+        }
+    }
+}
