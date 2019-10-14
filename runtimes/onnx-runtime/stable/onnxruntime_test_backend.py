f51e9e06de5256fb9e1924a3eba5ae910581378e
mitruska <katarzyna.mitrus@intel.com>
2019-10-15 00:54:09 +0200

    Excluded negative axis test

---
+++ runtimes/onnx-runtime/stable/onnxruntime_test_backend.py
@@ -0,0 +1,17 @@
+"""ONNX backend test initialization."""
+
+import unittest
+
+import onnxruntime.backend.backend as backend
+import onnx.backend.test
+
+
+# Import all test cases at global scope to make them visible to python.unittest
+backend_test = onnx.backend.test.BackendTest(backend, __name__)
+backend_test.exclude(r'test_compress_negative_axis[a-z,_]*')
+
+globals().update(backend_test.enable_report().test_cases)
+
+
+if __name__ == "__main__":
+    unittest.main()
