0877c944720399b61e8f70cef21ad3d2db7439e4
mitruska <katarzyna.mitrus@intel.com>
2019-10-14 23:55:24 +0200

    only onnxruntime

---
+++ test/onnxruntime_test_backend.py
@@ -0,0 +1,24 @@
+"""ONNX backend test initialization."""
+
+import importlib
+import test
+import unittest
+
+import onnxruntime.backend.backend as backend
+import onnx.backend.test
+
+
+# Set backend device name to be used
+backend.backend_name = "CPU"
+
+# This is a pytest variable to load extra plugins
+# Enable the ONNX compatibility report
+pytest_plugins = "onnx.backend.test.report"
+
+# Import all test cases at global scope to make them visible to python.unittest
+backend_test = onnx.backend.test.BackendTest(backend, __name__)
+globals().update(backend_test.enable_report().test_cases)
+
+
+if __name__ == "__main__":
+    unittest.main()
