057550e6488c58c145abe5eb902a32a9fe4123cb
mitruska <katarzyna.mitrus@intel.com>
2019-10-15 08:10:24 +0200

    One test

--- onnxruntime_test_backend.py
+++ onnxruntime_test_backend.py
@@ -8,7 +8,7 @@ import onnx.backend.test
 
 # Import all test cases at global scope to make them visible to python.unittest
 backend_test = onnx.backend.test.BackendTest(backend, __name__)
-backend_test.exclude(r'test_compress_negative_axis[a-z,_]*')
+# backend_test.exclude(r'test_compress_negative_axis[a-z,_]*')
 
 globals().update(backend_test.enable_report().test_cases)
 
